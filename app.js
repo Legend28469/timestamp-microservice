dc2709ed450c6c840bdfbba3a01355e283aafeb5
Johnathan Brown <johnathaneb@gmail.com>
2016-08-12 01:24:52 -0400

    Added a few comments for conversion

--- app.js
+++ app.js
@@ -14,18 +14,21 @@ app.get('/', function (req, res) {
 app.get("/:date", function (req, res) {
     var formattedDate = req.params.date;
 
+    // Check for number and return date
     if (parseInt(formattedDate)) {
         var newDate = moment.unix(formattedDate).format("MMMM DD, YYYY");
         res.json({
             unix: Number(formattedDate),
             natural: newDate
         });
+    // Check for date and return unix time
     } else if (moment(formattedDate).isValid()) {
         var unixTime = moment(formattedDate).format("X");
         res.json({
             unix: unixTime,
             natural: moment(formattedDate).format("MMMM DD, YYYY")
         });
+    // Invalid input so return null
     } else {
         res.json({
             unix: null,
