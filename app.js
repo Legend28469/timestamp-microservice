a124c50b6729d743df465c7f274170485ebc98c0
Johnathan Brown <johnathaneb@gmail.com>
2016-08-12 00:52:54 -0400

    Fixed Port settings for Heroku

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.get("/:date", function (req, res) {
     }
 });
 
-app.listen(3000, function () {
-    console.log('App listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('App Started');
 });
