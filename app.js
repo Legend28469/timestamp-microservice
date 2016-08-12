b18cb286f0dd27b7c0261eefc6ef3e6ace0a598c
Johnathan Brown <johnathaneb@gmail.com>
2016-08-11 23:19:03 -0400

    Added functionality to convert unix time to date

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var path = require("path");
+var moment = require("moment");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/index.html"), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+app.get("/:date", function (req, res) {
+    var formattedDate = req.params.date;
+
+    if (parseInt(formattedDate)) {
+        var newDate = moment.unix(formattedDate).format("MMMM DD, YYYY");
+        res.json({
+            unix: formattedDate,
+            natural: newDate
+        });
+    } else {
+        res.json({
+            unix: "Test",
+            natural: "Test"
+        });
+    }
 });
 
 app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+    console.log('App listening on port 3000!');
 });
